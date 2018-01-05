88ecc6766d8aa575ce0b95ac4b74e7c3119d1c93
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-06 00:49:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require('dotenv').config();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    env = require('./env'),
+    app = express().use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach((entry) => {
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.get('/webhook', (req, res) => {
+
+  let VERIFY_TOKEN = "upandup"
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
