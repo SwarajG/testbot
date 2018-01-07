09e803f70f5c3f919c1e20f67bd1b605a01ee882
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-07 16:17:23 +0530

    test

--- ApiHandlers/HandlePostback.js
+++ ApiHandlers/HandlePostback.js
@@ -5,7 +5,7 @@ module.exports = (senderPsid, receivedPostback) => {
   const { payload } = receivedPostback;
   console.log(receivedPostback);
   if (payload === 'getstarted') {
-    callSendAPI(GET_STARTED_RESPONSE);
+    callSendAPI(senderPsid, GET_STARTED_RESPONSE);
     console.log('Send some starting stuff...');
   }
   console.log('Postback: ', senderPsid, receivedPostback);
