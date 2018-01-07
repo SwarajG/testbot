b84e85dc7091cc946e6853a1e8a1378ca71cf602
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-08 01:01:21 +0530

    menu images changed

--- ApiHandlers/Response/Menu/MenuInfo.js
+++ ApiHandlers/Response/Menu/MenuInfo.js
@@ -1,6 +1,6 @@
 module.exports = [{
   name: 'Combos',
-  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-biryani.jpg',
+  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/combo.jpeg',
   button: {
     title: 'Show more',
     payload: 'show_more_combos',
@@ -14,21 +14,21 @@ module.exports = [{
   },
 }, {
   name: 'Rolls',
-  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-biryani.jpg',
+  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-biryani-roll.jpg',
   button: {
     title: 'Show more',
     payload: 'show_more_rolls',
   },
 }, {
   name: 'Dessert',
-  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-biryani.jpg',
+  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-phirni.jpg',
   button: {
     title: 'Show more',
     payload: 'show_more_dessert',
   },
 }, {
   name: 'Beverages',
-  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-biryani.jpg',
+  imageUrl: 'https://s3-ap-southeast-1.amazonaws.com/upandup-resources/upmup-chicksoup.jpg',
   button: {
     title: 'Show more',
     payload: 'show_more_beverages',
