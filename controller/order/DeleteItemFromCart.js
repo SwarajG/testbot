f8250388ebc98aa9e49b74be0a95ff18378ed3b4
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-21 17:05:17 +0530

    test

--- controller/order/DeleteItemFromCart.js
+++ controller/order/DeleteItemFromCart.js
@@ -6,14 +6,16 @@ const deleteItemFromCart = (userId, itemId, cb) => {
     if (err) {
       console.log('Error in getting order...');
     } else {
+      console.log(itemId);
       const order = orderList[0];
       const orderObjectId = order._id;
       const newOrder = _.cloneDeep(order);
       const newItemList = newOrder.itemList.filter(item => item.itemId !== itemId);
       newOrder.itemList = newItemList;
+      console.log(newOrder);
       Order
         .update(orderObjectId, newOrder)
-        .then(cb)
+        .then(response => cb(null, response))
         .catch(updateErr => console.log('Error in updating order for user...', updateErr));
     }
   });
