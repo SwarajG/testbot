478c74b5f2d270918ee7ccde9aa4a89810d5da78
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-21 13:58:01 +0530

    test

--- ApiHandlers/HandleMessage.js
+++ ApiHandlers/HandleMessage.js
@@ -5,25 +5,24 @@ module.exports = (senderPsid, receivedMessage) => {
   let response;
   console.log(receivedMessage);
   if (receivedMessage.quick_reply) {
-    console.log('reply', receivedMessage.quick_reply);
     const quickReply = receivedMessage.quick_reply;
     if (quickReply.payload) {
-      console.log('payload: ', quickReply.payload);
-      const paylod = quickReply.payload;
-      const splitesMessage = paylod.split('_');
+      const { payload } = quickReply;
+      const splitesMessage = payload.split('_');
       if (splitesMessage[0] === 'quantity') {
-        orderController.handleOrderState(senderPsid, quickReply, (err) => {
+        orderController.handleOrderState(senderPsid, payload, (err) => {
           if (err) {
             console.log('Sorry, not able to update to cart...', err);
           }
           console.log('Successfully updated to the cart...');
         });
       }
-    } else {
-      response = {
-        text: 'hello',
-      };
     }
+    // else {
+    //   response = {
+    //     text: 'hello',
+    //   };
+    // }
   }
   callSendAPI(senderPsid, response);
 };
