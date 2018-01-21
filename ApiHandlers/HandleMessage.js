1a0106e0525db193d636c480b388270d55584804
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-01-21 16:39:10 +0530

    test

--- ApiHandlers/HandleMessage.js
+++ ApiHandlers/HandleMessage.js
@@ -7,50 +7,52 @@ const { getResponseForReply } = require('./HandlePostback');
 const enums = require('../utils/enum');
 const Order = require('../model/order');
 
+const getElemets = order => order.itemList.map((item) => {
+  const itemImage = helper.getItemById(item.itemId).image;
+  const {
+    itemId,
+    name,
+    price,
+    quantity,
+  } = item;
+  return {
+    title: name,
+    subtitle: `Price: Rs. ${price}, Quantity: ${quantity}`,
+    image_url: itemImage,
+    buttons: [{
+      type: 'postback',
+      title: 'Place Order',
+      payload: 'place-order',
+    }, {
+      type: 'postback',
+      title: 'Change Quantity',
+      payload: `${enums.CHANGE_QUANTITY}_${itemId}`,
+    }, {
+      type: 'postback',
+      title: 'Remove from cart',
+      payload: `${enums.DELETE_ITEM}`,
+    }],
+  };
+});
+
 const showCurrentOrderCart = (userId) => {
   Order.getOpenOrderByUserId(userId, (err, orderList) => {
-    const order = orderList[0];
-    const response = {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: order.itemList.map((item) => {
-            const itemImage = helper.getItemById(item.itemId).image;
-            const {
-              itemId,
-              name,
-              price,
-              quantity,
-            } = item;
-            return {
-              title: name,
-              subtitle: `Price: ${price}Rs, Quantity: ${quantity}`,
-              image_url: itemImage,
-              buttons: [{
-                type: 'postback',
-                title: 'Place Order',
-                payload: 'place-order',
-              }, {
-                type: 'postback',
-                title: 'Change Quantity',
-                payload: `${enums.CHANGE_QUANTITY}_${itemId}`,
-              }, {
-                type: 'postback',
-                title: 'Remove from cart',
-                payload: `${enums.DELETE_ITEM}`,
-              }, {
-                type: 'postback',
-                title: 'Back to menu',
-                payload: 'show-menu',
-              }],
-            };
-          }),
+    if (err) {
+      console.log('Sorry, not able to get the order...');
+    } else {
+      const order = orderList[0];
+      const response = {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'generic',
+            elements: getElemets(order),
+          },
         },
-      },
-    };
-    console.log(response, JSON.stringify(response.attachment.payload.elements));
-    callSendAPI(userId, response);
+      };
+      console.log(response, JSON.stringify(response.attachment.payload.elements));
+      callSendAPI(userId, response);
+    }
   });
 };
 
@@ -60,7 +62,6 @@ module.exports = (senderPsid, receivedMessage) => {
     const quickReply = receivedMessage.quick_reply;
     const { payload } = quickReply;
     if (payload) {
-      console.log(payload);
       if (payload === 'show-menu') {
         const newResponse = getResponseForReply(payload, senderPsid);
         if (newResponse) {
