38184e60d2c6efd5baddd663085cdb1feec18898
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-02-15 23:13:06 +0530

    test

--- ApiHandlers/SendEmail.js
+++ ApiHandlers/SendEmail.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 module.exports = async (userId, order) => {
   const { PAGE_ACCESS_TOKEN } = process.env;
   try {
-    const fbUserResponse = await fetch(`https://graph.facebook.com/v2.6/${senderPsid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`);
+    const fbUserResponse = await fetch(`https://graph.facebook.com/v2.6/${userId}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`);
     const userJsonResponse = await fbUserResponse.json();
     sendEmailToaAdmin(order, userJsonResponse);
   } catch (error) {
