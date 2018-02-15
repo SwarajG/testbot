998a814c676f6394bead631e28f54133a9f57025
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-02-15 22:38:50 +0530

    test

--- ApiHandlers/Response/Menu.js
+++ ApiHandlers/Response/Menu.js
@@ -1,6 +1,7 @@
 const priceList = require('./Price');
 const Order = require('../../model/order');
 const callSendAPI = require('../CallSendAPI');
+const _ = require('lodash');
 
 const menuList = priceList.menu.map((category) => {
   const { name, imageUrl, button } = category;
@@ -16,26 +17,35 @@ const menuList = priceList.menu.map((category) => {
   };
 });
 
-const getResponse = response => ({
+const responseObject = {
   attachment: {
     type: 'template',
     payload: {
       template_type: 'generic',
-      elements: response,
+      elements: {},
     },
   },
-});
+};
+
+const getResponse = (response) => {
+  const clonedResponse = _.cloneDeep(response);
+  const clonedResponseObject = _.cloneDeep(responseObject);
+  clonedResponseObject.attachment.payload.elements = clonedResponse;
+  return clonedResponseObject;
+};
 
 module.exports = (userId) => {
   Order.getOpenOrderByUserId(userId, (err, orderList) => {
     if (err) {
       console.log('Sorry, not able to get the order...');
     } else if (orderList.length === 0) {
-      const response = getResponse(menuList);
+      const clonedMenuList = _.cloneDeep(menuList);
+      const response = getResponse(clonedMenuList);
       console.log('1', JSON.stringify(response));
       callSendAPI(userId, response);
     } else if (orderList.length > 0) {
-      const newResponse = menuList.map((menuItem) => {
+      const clonedMenuList = _.cloneDeep(menuList);
+      const newResponse = clonedMenuList.map((menuItem) => {
         menuItem.buttons.push({
           title: 'Place order',
           payload: 'place-order',
