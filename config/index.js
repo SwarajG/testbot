f741a18876853cffe0f847141d3a02cf76f9a4e5
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-02-18 01:08:25 +0530

    test

--- config/index.js
+++ config/index.js
@@ -1,16 +1,8 @@
 const mongodbUser = process.env.MONGO_USER;
 const mongodbPWd = process.env.MONGO_PWD;
-const env = process.env.NODE_ENV;
-const mongodbDataBase = 'upandup-chatbot';
+const mongodbDataBase = process.env.DB_NAME;
 const mongodbConnection = `${process.env.MONGO_URL}:${process.env.MONGO_PORT}`;
-let mongodbUrl;
-
-if (env === 'prod') {
-  mongodbUrl = `mongodb://${mongodbUser}:${mongodbPWd}@${mongodbConnection}/${mongodbDataBase}`;
-} else {
-  mongodbUrl = `mongodb+srv://${mongodbUser}:${mongodbPWd}@${process.env.MONGO_URL}/${mongodbDataBase}`;
-}
 
 module.exports = {
-  mongodbUrl,
+  mongodbUrl: `mongodb://${mongodbUser}:${mongodbPWd}@${mongodbConnection}/${mongodbDataBase}`,
 };
