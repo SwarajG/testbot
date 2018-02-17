2a7a30daa72c804de9276eb6884873044082ee5f
SwarajG <gandhiswaraj9067008148@gmail.com>
2018-02-17 23:51:52 +0530

    prod first commit

--- config/index.js
+++ config/index.js
@@ -1,8 +1,15 @@
 const mongodbUser = process.env.MONGO_USER;
 const mongodbPWd = process.env.MONGO_PWD;
 const mongodbDataBase = 'upandup-chatbot';
-const mongodbConnection = 'ds245277.mlab.com:45277';
+const mongodbConnection = `${process.env.MONGO_URL}:${process.env.MONGO_PORT}`;
+let mongodbUrl;
+
+if (mongodbUser && mongodbPWd) {
+  mongodbUrl = `mongodb://${mongodbUser}:${mongodbPWd}@${mongodbConnection}/${mongodbDataBase}`;
+} else {
+  mongodbUrl = `mongodb://${mongodbConnection}/${mongodbDataBase}`;
+}
 
 module.exports = {
-  mongodbUrl: `mongodb://${mongodbUser}:${mongodbPWd}@${mongodbConnection}/${mongodbDataBase}`,
+  mongodbUrl,
 };
